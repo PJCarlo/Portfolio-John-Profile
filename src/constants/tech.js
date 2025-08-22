b2b9db428e436e5edfd9c503f87f7f26ccc2a4bb
jayce <pantoniajohncarlo1912@gmail.com>
2025-08-22 15:14:35 +0800

    Code Updated!

--- src/constants/tech.js
+++ src/constants/tech.js
@@ -1,10 +1,11 @@
-import { RiHtml5Line, RiCss3Line, RiJavascriptLine, RiReactjsLine, RiTailwindCssLine, RiNodejsLine } from "react-icons/ri";
+import { RiHtml5Line, RiCss3Line, RiJavascriptLine, RiNextjsLine, RiReactjsLine, RiTailwindCssLine, RiNodejsLine } from "react-icons/ri";
 import { SiMysql, SiMongodb, SiFlask, SiPhp, SiGit } from "react-icons/si";
 
 export const TECH_STACK = [
     { icons: RiHtml5Line, color: "text-orange-500"},
     { icons: RiCss3Line, color: "text-blue-500"},
     { icons: RiJavascriptLine, color: "text-yellow-500"},
+    { icons: RiNextjsLine, color: "text-gray-300"},
     { icons: RiReactjsLine, color: "text-cyan-400"},
     { icons: RiTailwindCssLine, color: "text-sky-500"},
     { icons: RiNodejsLine, color: "text-green-500"},
